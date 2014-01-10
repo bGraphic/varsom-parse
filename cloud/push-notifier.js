44a5f2bedb8b313cae2ea46ff9172600539d8011
Benedicte Raae <raae@bgraphic.no>
2014-01-10 09:57:57 +0100

    Fix for push, only compare warnings with same validFrom and validTo fields
    Signed-off-by: Benedicte Raae <raae@bgraphic.no>

--- cloud/push-notifier.js
+++ cloud/push-notifier.js
@@ -8,6 +8,10 @@ var _ = require('underscore'),
     SIGNIFICANT_CHANGE_THRESHOLD = 1;
 
 
+function isSameWarning(newWarning, oldWarning) {
+    return newWarning.get('validFrom').getTime() === oldWarning.validFrom.time && newWarning.get('validTo').getTime() === oldWarning.validTo.time;
+}
+
 function findWarningLevel(warning) {
     return (warning.has('dangerLevel')) ? warning.get('dangerLevel')
         : (warning.has('activityLevel')) ? warning.get('activityLevel')
@@ -50,50 +54,62 @@ function pushUpdates(area, warningType, oldForecast, newForecast) {
         areaID = areaIDForArea(area);
 
     promise = promise.then(function () {
-        var changedWarning = _.find(newForecast, function (warning, index) {
-            if (warningLevelHasChanged(warning, oldForecast[index])) {
-                if (index < 2
-                        || (index === 2
-                            && warningLevelHasChangedSignificantly(warning, oldForecast[index])
-                           )
-                        ) {
+
+        var combinedForecasts = _.map(newForecast, function (newWarning) {
+            var oldWarning = _.find(function (warning) {
+                if (isSameWarning(newWarning, warning)) {
                     return warning;
+                }
+            });
+            return { oldWarning: oldWarning, newWarning: newWarning };
+        });
+
+        var changedWarning = _.find(combinedForecasts, function (combinedForecast, index) {
+            var oldWarning = combinedForecast.oldWarning,
+                newWarning = combinedForecast.newWarning;
+
+            if (warningLevelHasChanged(newWarning, oldWarning)) {
+                if (index < 2 || (index === 2 && warningLevelHasChangedSignificantly(newWarning, oldWarning))) {
+                    return true;
                 }
             }
         });
 
+        if (changedWarning && areaID !== -1) {
+            if (changedWarning !== undefined && areaID !== -1) {
+                var locArgs = [],
+                    locArgsOldTemp = [],
+                    locArgsNewTemp = [];
 
-        if (changedWarning !== undefined && areaID !== -1) {
-            var locArgs = [],
-                locArgsOldTemp = [],
-                locArgsNewTemp = [];
+                _.each(combinedForecasts, function (combinedForecast, index) {
+                    var oldWarning = combinedForecast.oldWarning,
+                        newWarning = combinedForecast.newWarning;
+                    locArgsOldTemp.push(findOldWarningLevel(oldWarning));
+                    locArgsNewTemp.push(findWarningLevel(newWarning));
+                });
 
-            _.each(newForecast, function (newWarning, index) {
-                locArgsOldTemp.push(findOldWarningLevel(oldForecast[index]));
-                locArgsNewTemp.push(findWarningLevel(newWarning));
-            });
+                locArgs.push(area.get("name"));
+                locArgs = locArgs.concat(locArgsOldTemp).concat(locArgsNewTemp);
 
-            locArgs.push(area.get("name"));
-            locArgs = locArgs.concat(locArgsOldTemp).concat(locArgsNewTemp);
+                var pushQuery = new Parse.Query(Parse.Installation);
+                pushQuery.equalTo('deviceType', 'ios');
+                pushQuery.equalTo('channels', area.className + '-' + areaID);
+                return Parse.Push.send({
+                    where: pushQuery,
+                    data: {
+                        alert : {
+                            "loc-key": warningType + " forecast changed",
+                            "loc-args": locArgs
+                        },
+                        warningType: warningType,
+                        areaType: area.className,
+                        areaId: areaID
+                    }
+                });
+            }
 
-            var pushQuery = new Parse.Query(Parse.Installation);
-            pushQuery.equalTo('deviceType', 'ios');
-            pushQuery.equalTo('channels', area.className + '-' + areaID);
-            return Parse.Push.send({
-                where: pushQuery,
-                data: {
-                    alert : {
-                        "loc-key": warningType + " forecast changed",
-                        "loc-args": locArgs
-                    },
-                    warningType: warningType,
-                    areaType: area.className,
-                    areaId: areaID
-                }
-            });
+            return Parse.Promise.as();
         }
-
-        return Parse.Promise.as();
     });
 
     return promise;
@@ -102,3 +118,48 @@ function pushUpdates(area, warningType, oldForecast, newForecast) {
 module.exports = {
     pushUpdates: pushUpdates
 };
+
+//        var changedWarning = _.find(newForecast, function (warning, index) {
+//            if (warningLevelHasChanged(warning, oldForecast[index])) {
+//                if (index < 2
+//                        || (index === 2
+//                            && warningLevelHasChangedSignificantly(warning, oldForecast[index])
+//                           )
+//                        ) {
+//                    return warning;
+//                }
+//            }
+//        });
+//
+//
+//        if (changedWarning !== undefined && areaID !== -1) {
+//            var locArgs = [],
+//                locArgsOldTemp = [],
+//                locArgsNewTemp = [];
+//
+//            _.each(newForecast, function (newWarning, index) {
+//                locArgsOldTemp.push(findOldWarningLevel(oldForecast[index]));
+//                locArgsNewTemp.push(findWarningLevel(newWarning));
+//            });
+//
+//            locArgs.push(area.get("name"));
+//            locArgs = locArgs.concat(locArgsOldTemp).concat(locArgsNewTemp);
+//
+//            var pushQuery = new Parse.Query(Parse.Installation);
+//            pushQuery.equalTo('deviceType', 'ios');
+//            pushQuery.equalTo('channels', area.className + '-' + areaID);
+//            return Parse.Push.send({
+//                where: pushQuery,
+//                data: {
+//                    alert : {
+//                        "loc-key": warningType + " forecast changed",
+//                        "loc-args": locArgs
+//                    },
+//                    warningType: warningType,
+//                    areaType: area.className,
+//                    areaId: areaID
+//                }
+//            });
+//        }
+//
+//        return Parse.Promise.as();
