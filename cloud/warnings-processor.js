467d166d58fa372173560f0e5e4aad9da5080aaf
Benedicte Raae <raae@bgraphic.no>
2014-10-31 12:11:59 +0100

    Fix use of reserved word

--- cloud/warnings-processor.js
+++ cloud/warnings-processor.js
@@ -100,9 +100,9 @@ function highestForecastLevel(forecast) {
     return highestForecastLevel;
 }
 
-function avalancheProblemHasChanged(existing, new) {
-  return existing.causeId !== new.causeId
-    || existing.extId !== new.extId;
+function avalancheProblemHasChanged(existingProblem, newProblem) {
+  return existingProblem.causeId !== newProblem.causeId
+    || existingProblem.extId !== newProblem.extId;
 }
 
 function highestPriorityAvalancheProblemHasChanged(currentForecast, newForecast) {
