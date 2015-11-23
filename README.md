bbb00cbe9b839a7e73641d39143d1c614f2b8c07
Benedicte Raae <raae@bgraphic.no>
2015-11-23 12:26:04 +0100

    Refactored api fetch

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 varsom-parse
 ============
 
-Parse backend til varsom-ios, står for import fra api og senere også pushvarsling.
+Parse backend for varsom-ios and varsom-hybrid. 
+* Imports warnings from the api.nve.no
+* Manages push notofocations
+
+= Config = 
+
+== Import ==
+The api urls to import from is managed by Parse.Config.
+
+=== Test api import configurations ===
+* _avalancheApiUrl_: http://h-web03.nve.no/Avalanche_Test/api/RegionSummary/Detail/1
+* _floodApiUrl_: http://h-web03.nve.no/flood_test/api/CountySummary/1
+* _landslideApiUrl_: http://h-web03.nve.no/landslide_test/api/CountySummary/1
--- cloud/nve-warnings-api-handler.js
+++ cloud/nve-warnings-api-handler.js
@@ -8,47 +8,44 @@ var WARNING_TYPE_FLOOD = "flood";
 var WARNING_TYPE_LANDSLIDE = "landslide";
 
 function apiUrl(warningType) {
-  return Parse.Config.get().then(function(config) {
-    return config.get(warningType + "ApiUrl");
+  return Parse.Config.get().then(function (config) {
+    var apiUrl = config.get(warningType + "ApiUrl");
+    if(apiUrl) {
+      return apiUrl;
+    } else {
+      return Parse.Promise.error("Missing Parse.Config parameter: " + warningType + "ApiUrl");
+    }
   });
 }
 
-function api(url) {
-    return Parse.Cloud.httpRequest({
-        url: url,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+function fetchDataFromUrl(url) {
+  return Parse.Cloud.httpRequest({
+    url: url,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).done(function (result) {
+    return result.data;
+  }).fail(function (error) {
+    console.error("API Error: " + JSON.stringify(error));
+    return Parse.Promise.error("API Error: " + url);
+  });
 }
 
-function fetch(url) {
-    var promise = new Parse.Promise();
-
-    api(url).then(function(result) {
-        promise.resolve(result.data);
-    }, function (error) {
-        console.error("API Error: " + JSON.stringify(error));
-        promise.reject(error);
-    });
-
-    return promise;
-}
-
-function fetchWarningsForWarningType(warningType) {
-    return apiUrl(warningType).then(function (url) {
-        return fetch(url);
-    });
+function fetchWarningsJsonForWarningType(warningType) {
+  return apiUrl(warningType).then(function (url) {
+    return fetchDataFromUrl(url);
+  });
 }
 
 module.exports = {
-    fetchAvalancheWarnings: function() {
-      return fetchWarningsForWarningType(WARNING_TYPE_AVALANCHE);
-    },
-    fetchFloodWarnings: function() {
-      return fetchWarningsForWarningType(WARNING_TYPE_FLOOD);
-    },
-    fetchLandSlideWarnings: function() {
-      return fetchWarningsForWarningType(WARNING_TYPE_LANDSLIDE);
-    },
+  fetchAvalancheWarnings: function () {
+    return fetchWarningsJsonForWarningType(WARNING_TYPE_AVALANCHE);
+  },
+  fetchFloodWarnings: function () {
+    return fetchWarningsJsonForWarningType(WARNING_TYPE_FLOOD);
+  },
+  fetchLandSlideWarnings: function () {
+    return fetchWarningsJsonForWarningType(WARNING_TYPE_LANDSLIDE);
+  }
 };
